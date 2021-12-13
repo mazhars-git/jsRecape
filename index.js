6ce4d1d603537e340378d5e4d27d9467ee27b234
Md. Mazharul Islam <mazharulislam611@gmail.com>
2021-12-13 22:33:27 +0600

    module 12/6

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* 
 var mobilePrice = 2550;
 console.log(mobilePrice);
 
@@ -19,4 +20,37 @@ var myName = "azad khan";
 myName = "Azad Vhi";
 
 console.log(myname)
+
+
+
+var myAuth = "I take shelter of Allah from the rejected Shaitan";
+
+var lower = myAuth.toLowerCase();
+var Upper = myAuth.toUpperCase();
+var Split = myAuth.split(" ");
+
+var position = myAuth.indexOf("The");
+
+console.log(lower, Upper, position, Split);
+*/
+
+var number1 = 0.1;
+var number2 = 0.2;
+
+// number1 = '' + number1;
+
+// number2 = parseFloat(number2);
+// number2 = parseInt(number2);
+// number2 = +number2;
+
+// console.log(number1 + number2)
+
+total = (number1 + number2);
+total = total.toFixed(3);
+
+console.log(total)
+
+
+
+
 
