60614a3af43ac4ebcd1465ef1cea6135c71b9c8b
Md. Mazharul Islam <mazharulislam611@gmail.com>
2021-12-13 21:09:43 +0600

    variable

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 var mobilePrice = 2550;
 console.log(mobilePrice);
+
+var amountOfPen = 200;
+console.log(typeof amountOfPen);
+
+var friendName = "Ahmad Abdullah";
+console.log(typeof friendName);
+
+var isGood = true;
+var isBad = false;
+
+console.log(typeof isGood)
+console.log(isBad)
+
+
+var myName = "azad khan";
+
+myName = "Azad Vhi";
+
+console.log(myname)
+
