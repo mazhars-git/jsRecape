9050c181f15c53a9b0355fbb6482e9832737856b
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-07-25 23:10:32 +0600

    update recape

---
+++ module4-17-5.js
@@ -0,0 +1,19 @@
+var penColor = 'red';
+var quantity = 2;
+var isActive = false;
+
+// array
+
+var items = ['watch', 'cap', 'shows', 'mobile'];
+
+items.indexOf();
+
+items.push('table');
+
+items.pop();
+
+if (condition) {
+    
+} else {
+    
+}
