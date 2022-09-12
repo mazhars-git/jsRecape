8c51ed3bb551e08aafae3459cfe1107c8737fc4f
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-09-12 08:41:42 +0600

    function

---
+++ function.js
@@ -0,0 +1,9 @@
+function switchOn(){
+    console.log('switch on the Fan');
+}
+
+switchOn();
+console.log('Amar Gorom Lagche');
+console.log('Oi Fan ta bondho kor');
+
+switchOn();
---
+++ module4-19-1.js
@@ -0,0 +1,17 @@
+//variable
+var name = 'Zayed';
+
+
+var friendName = ['Azad', 'Kashem', 'Kaja', 'Alal', 'Jamal'];
+
+var kajaPosition = friendName.indexOf('Kaja');
+
+//change name
+friendName[2] = 'Abul';
+
+//enter new item
+
+friendName.push('Nazim');
+//remove
+friendName.pop();
+
