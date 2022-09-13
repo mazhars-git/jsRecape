e0c30baf4b34f3cfa299f01cc21a6f60eda32411
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-09-13 07:37:59 +0600

    multiple function

--- function.js
+++ function.js
@@ -1,9 +1,21 @@
 function switchOn(){
-    console.log('switch on the Fan');
+    // console.log('switch on the Fan');
 }
 
 switchOn();
-console.log('Amar Gorom Lagche');
-console.log('Oi Fan ta bondho kor');
+// console.log('Amar Gorom Lagche');
+// console.log('Oi Fan ta bondho kor');
 
 switchOn();
+
+function yourApple(Taka) {
+    var applePrice = 20;
+    var myApple = Taka / applePrice;
+
+    return myApple;
+}
+
+var apple = yourApple(360);
+
+console.log(apple);
+
---
+++ multiple-parameter.js
@@ -0,0 +1,17 @@
+function addToNum(num1, num2){
+    var total = num1 + num2;
+    return total;
+}
+
+var result = addToNum(201, 255);
+
+console.log('Add:', result);
+
+
+function biyogKora(num1, num2){
+    var biyogFall = num1 - num2;
+    return biyogFall;
+}
+
+var result = biyogKora(55, 40);
+console.log("biyog; ", result);
