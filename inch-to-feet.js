56588f421f53f0c1a5d55c9a9da4f98f0426202d
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-09-17 20:13:30 +0600

    converter

---
+++ inch-to-feet.js
@@ -0,0 +1,11 @@
+
+function inchToFeet(inches){
+    var feet = inches/12;
+
+    return feet;
+}
+
+var totalFeet = inchToFeet(125);
+
+console.log(totalFeet);
+
--- others.js
+++ others.js
@@ -3,9 +3,11 @@ var friends = ['Jasim', 'Samad', 'Saddam', 'Junayed', 'Kabul', 'Abul'];
 //check is exists in array
 
 if(friends.indexOf('Jasim') == !-1){
-    console.log("It is available here");
+    // console.log("It is available here");
 }
 if(friends.indexOf('Jalal') == -1){
-    console.log('It is not available');
+    // console.log('It is not available');
 }
+
+
 
